ebfd1a236a826de0ca4d5d83ad5f8505f441b885
Doan Dinh <doandoinh@usf.edu>
2025-06-15 14:24:57 -0400

    added product inventory array with name, category, price, and stock

---
+++ cc_4.js
@@ -0,0 +1,7 @@
+const stockList = [
+  { name: "Sonos Roam Speaker", category: "electronics", price: 179.00, inventory: 4 },
+  { name: "Adidas Ultraboost Shoes", category: "apparel", price: 190.00, inventory: 6 },
+  { name: "Almonds", category: "groceries", price: 8.49, inventory: 15 },
+  { name: "Method All-Purpose Cleaner", category: "household", price: 5.25, inventory: 20 },
+  { name: "Sharpie Pen Set", category: "education", price: 11.99, inventory: 10 }
+];
